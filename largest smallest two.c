84a11d84f3a62e6f6e67b6bf36c042361971b1c6
vinay100ni <vinayso457@gmail.com>
2025-08-14 15:20:55 +0530

    comparision of two no.

---
+++ largest smallest two.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+int a,b;
+printf("ënter first number:");
+scanf("%d",&a);
+printf("Enter second number");
+scanf("%d",&b);
+if(a>b)
+{
+printf("%d is greator than %d",a,b);
+
+}else
+{
+    printf("%d is greator than %d",b,a);
+}
+return 0;
+}
