b342021eeb65d095e4f5ccbb379c2ce42611f8cb
vinay100ni <vinayso457@gmail.com>
2025-08-14 14:59:39 +0530

    Add files via upload

---
+++ vinayece.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+int a;
+printf("ënter your marks:");
+scanf("%d",&a);
+
+if(a>90)
+{
+    printf("your grade is: A+");
+}else
+{
+    printf("general walo ko admission nhi milta");
+}
+return 0;
+}
